242611934098498720ce0dfd0b48443a152671ee
LiukSkaiWo <albergo.vargroup@gmail.com>
2023-08-23 11:23:33 +0200

    Added blank code

---
+++ src/test/java/it/xa/example/Batch/Service/BatchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package it.xa.example.Batch.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
